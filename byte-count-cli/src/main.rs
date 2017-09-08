7477d6ca57b00cb19716d6cb8b1d6109edb568a8
Mick van Gelderen <mickvangelderen@gmail.com>
2017-09-08 14:01:56 +0200

    :wrench: Count bytes in file
    File path must be supplied as the first argument.

--- byte-count-cli/src/main.rs
+++ byte-count-cli/src/main.rs
@@ -1,6 +1,8 @@
 extern crate byte_count;
 
 use byte_count::byte_count;
+use std::error::Error;
+use std::io::Read;
 
 fn print_byte_count(counts: &[u32; 0x100]) {
     for (byte, &count) in counts
@@ -13,5 +15,21 @@ fn print_byte_count(counts: &[u32; 0x100]) {
 }
 
 fn main() {
-    print_byte_count(&byte_count(&[0, 1, 1, 2, 4, 100, 100]));
+    let path = match std::env::args_os().skip(1).next() {
+        Some(value) => std::path::PathBuf::from(value),
+        None => panic!("Pass a file path to count the bytes from"),
+    };
+
+    let mut file = match std::fs::File::open(&path) {
+        Err(err) => panic!("Failed to open {}: {}", path.display(), err.description()),
+        Ok(file) => file
+    };
+
+    let mut contents = String::new();
+    match file.read_to_string(&mut contents) {
+        Err(err) => panic!("Failed to read {}: {}", path.display(), err.description()),
+        Ok(contents) => contents
+    };
+
+    print_byte_count(&byte_count(&contents.into_bytes()));
 }
